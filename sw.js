7a0b638875f976bdd89a3b3136a29882265c7dc9
kubok758 <artemborovitskiy0312@gmail.com>
2026-01-19 17:16:02 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'car-rando-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/mercedes.jpg',
+  '/bmw.jpg',
+  '/porsche.jpg',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
