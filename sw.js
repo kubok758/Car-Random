8decbc3bd97c8fd87ce946300ebdcaeb16fbbb67
kubok758 <artemborovitskiy0312@gmail.com>
2026-01-20 10:58:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'car-rando-v11';
+const CACHE_NAME = 'car-rando-v25';
 const ASSETS = [
   '/',
   '/index.html',
@@ -11,28 +11,17 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+  e.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
+    if (key !== CACHE_NAME) return caches.delete(key);
+  }))));
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
-
